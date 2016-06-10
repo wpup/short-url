c3dd74f800421c1c3480957da1f4f4e5b1eb1dbd
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-06-10 07:39:58 +0200

    Add missing return tag

--- short-url.php
+++ short-url.php
@@ -4,7 +4,7 @@
  * Plugin Name: Short url
  * Description: Short url to the permalink, like Simple Address in EPiServer.
  * Author: Fredrik Forsmo
- * Author URI: https://frozzare.com
+ * Author URI: https://frozzare.com/
  * Version: 2.0.3
  * Plugin URI: https://github.com/frozzare/short-url
  */
@@ -15,6 +15,8 @@ require_once __DIR__ . '/src/functions.php';
 
 /**
  * Load Short Url plugin.
+ *
+ * @return Short_Url
  */
 add_action( 'plugins_loaded', function () {
 	return Short_Url::instance();
